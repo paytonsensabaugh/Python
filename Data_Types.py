c0410dec8edea08f63984a43b54a89611b1eea48
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-08-26 20:06:40 -0500

    Add files via upload

---
+++ Data_Types.py
@@ -0,0 +1,27 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 4
+print("Literal:", x)
+
+# Integer
+y = -10
+print("Integer:", y)
+
+# Float
+z = 24.8
+print("Float:", z)
+
+# String
+name = "Payton"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
+
+age=19
+print("My age is" ,age)
+
+is_active = True
+print("age:", is_active)
