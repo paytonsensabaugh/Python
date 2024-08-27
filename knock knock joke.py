9652265dab456a1e3a635adfb1cef8862aace0a8
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-08-26 19:48:43 -0500

    Add files via upload

---
+++ knock knock joke.py
@@ -0,0 +1,12 @@
+# Knock-Knock Joke Program
+print("Knock, knock.")  
+print("Who's there?")  
+print("Jess.")  
+print("Jess who?")  
+print("jess me and my shadow!")  
+
+print()
+# As one line of code using escape characters
+print("Knock, knock.\nWho's there?\nJess.\nJess who?\nJess me and my shadow!")
+
+# The purpose of the print program is to "print" specific messages on a screen
