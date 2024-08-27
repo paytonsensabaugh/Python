21d9acb7fb3288b7de2a405e58273151620d1b05
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-08-26 21:08:19 -0500

    Add files via upload

---
+++ basic operations.py
@@ -0,0 +1,14 @@
+#solving a mathmatical operation
+
+length = 10
+width = 20
+area = length * width 
+
+print("The area of the rectangle with a length of " + str(length) + " and a width of " + str(width) + " is " + str(area))
+
+#The area of the rectangle with a length of <length> and a width of <width> is <area>.
+
+
+#The purpose of stringing together numbers is to combine the program to complete a math equation.
+#I can see this being useful for programs that are designed for younger poeple to help learn math.
+#combinding both the "print" feaure and the "str" you can program complete equations out with one line of code.
