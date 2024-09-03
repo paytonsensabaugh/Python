eed3e0a9a5400b604ba6dbce5077813b49d675b9
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-09-02 21:55:12 -0500

    Add files via upload

---
+++ pythoncomments.py
@@ -0,0 +1,39 @@
+# calculating miles per gallon 
+# tank size of 15 
+# distance of 600 
+# mpa fomula = the distance / the tank_size 
+
+tank_size = 15
+distance = 600
+mpg = distance / tank_size
+print("Miles per gallon:", mpg)
+
+
+# calculating miles per gallon 
+# tank size of 15 
+# distance of 600 
+# mpa fomula = the distance / the tank_size
+
+'''
+calculating miles per gallon 
+tank size of 15 
+distance of 600 
+mpa fomula = the distance / the tank_size 
+''' 
+
+"""
+calculating miles per gallon 
+tank size of 15 
+distance of 600 
+mpa fomula = the distance / the tank_size 
+"""
+
+     # CTRL + / example 
+# calculating miles per gallon 
+# tank size of 15 
+# distance of 600 
+# mpa fomula = the distance / the tank_size 
+
+
+# inline comment
+x=10#  
