4fa08fac1a9df65cd939b91eb3797d09dde4609d
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-09-02 21:40:31 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,49 @@
+# Example  
+
+# Define variables for centimeter values
+cm_value_1 = 25.4
+cm_value_2 = 50.8
+cm_value_3 = 76.2
+cm_value_4 = 101.6
+
+# Conversion factor: 1 inch = 2.54 centimeters
+conversion_factor = 2.54
+
+# Calculate inches for each centimeter value
+inches_1 = cm_value_1 / conversion_factor
+inches_2 = cm_value_2 / conversion_factor
+inches_3 = cm_value_3 / conversion_factor
+inches_4 = cm_value_4 / conversion_factor
+
+# Output the results
+print(f"{cm_value_1} centimeters is equal to {inches_1:.2f} inches.")
+print(f"{cm_value_2} centimeters is equal to {inches_2:.2f} inches.")
+print(f"{cm_value_3} centimeters is equal to {inches_3:.2f} inches.")
+print(f"{cm_value_4} centimeters is equal to {inches_4:.2f} inches.")
+
+
+# Assignment 
+
+# Variables for Kilograms 
+kg_value_1 = 10
+kg_value_2 = 30
+kg_value_3 = 68
+kg_value_4 = 102
+kg_value_5 = 207
+
+# Conversion Factor: 1 kilo = 2.20462 pounds
+conversion_factor = 2.20462
+
+# Calculate Pounds for each Kilo
+pounds_1 = kg_value_1 * conversion_factor
+pounds_2 = kg_value_2 * conversion_factor 
+pounds_3 = kg_value_3 * conversion_factor
+pounds_4 = kg_value_4 * conversion_factor 
+pounds_5 = kg_value_5 * conversion_factor 
+
+# Outputs results 
+print(f"{kg_value_1} kilograms is equal to {pounds_1:.2f} pounds.")
+print(f"{kg_value_2} kilograms is equal to {pounds_2:.2f} pounds.") 
+print(f"{kg_value_3} kilograms is equal to {pounds_3:.2f} pounds.")
+print(f"{kg_value_4} kilograms is equal to {pounds_4:.2f} pounds.") 
+print(f"{kg_value_5} kilograms is equal to {pounds_5:.2f} pounds.")
