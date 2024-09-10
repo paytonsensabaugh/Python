0be7cc473a524a7d3b4a6c547bb0bfb04b68256f
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-09-09 21:46:39 -0500

    Add files via upload

---
+++ makingdecisionsinpython.py
@@ -0,0 +1,24 @@
+age = int(input("How old are you? : ")) 
+
+if age >= 16:
+    print("You are old enough to drive.")
+else:
+    print("You are not old enough to drive.")
+
+if age >= 18:
+    print("You can vote.")
+else:
+    print("You are not old enough to vote.")
+
+if age >= 21:
+    print("You can buy alcohol legally.")
+else:
+    print("You are not old enough to buy alcohol legally.")
+
+if age >= 65:
+    print("You are eligible for the senior discount.")
+else:
+    print("You are not eligible for the senior discount.")
+
+
+
