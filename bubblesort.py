ffdcf0bd8a850a3429e7c03269cc157a123b7741
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-09-15 20:53:52 -0500

    bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -1,26 +1,26 @@
-# empty list
+# empty list, says to have the user enter the names
 names = []
 
-# enter names
+# enter names, user enters the names into the terminal
 for i in range(5):
     name = input(f"Enter name {i+1}: ")
     names.append(name)
 
-# prints the names
+# prints the names, prints the names that the user inputed 
 print("\nOriginal list of names:")
 print(names)
 
-# bubble sort
+# bubble sort, sorts the names that the user has inputed 
 names.sort ()
 print (names)
 
-# prints sorted list 
+# prints sorted list, prints those names that were sorted 
 print("\nSorted list of names (ascending order):")
 print(names)
 
-# reverses the list
+# reverses the list, reverses the list 
 names.reverse()
 
-# Prints the list
+# Prints the list, prints the reversed list
 print("\nReversed list of names:")
 print(names)
