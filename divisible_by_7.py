fea27d9ad8bffe94c10a408058610a7f7f3ce27b
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-09-16 18:03:39 -0500

    Update divisible_by_7.py

--- divisible_by_7.py
+++ divisible_by_7.py
@@ -1,9 +1,1 @@
-#divisible by 7 
-#def main defines a function called main that you can then use in your script.
-def main(): 
-    for number in range(1, 301): #his loop iterates through numbers starting from 1 up to 300 (inclusive).
-        if number % 7 == 0: #The modulus operator % computes the remainder of number divided by 7. If the remainder is 0, the number is divisible by 7.
-            print(number) #If the condition is true, the number is printed.
-
-if __name__ == "__main__":   #used to determine whether the current script is being run as the main program or if it is being imported as a module into another script
-    main()
+print([number for number in range(7, 301, 7)])
