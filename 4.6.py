0b4087de61348d332495d0e74813ac8d135054b6
paytonsensabaugh <psensabaugh@students.mchenry.edu>
2024-09-29 11:54:36 -0500

    Add files via upload

---
+++ 4.6.py
@@ -0,0 +1,6 @@
+def main():
+    programming_class = ('Intro to Python', 'Advanced Python', 'Database Essentials', 'Web Development Basics', 'Data Structures in Python', 'Web Design Fundamentals')
+    for classes in programming_class:
+        print(classes)
+    print("These are the", len(programming_class), "programming classes")
+main()
